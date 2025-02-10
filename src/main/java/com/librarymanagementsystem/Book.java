4abdacfd524c347738570dcd802ad5c009a61025
yasith9898 <yasithdil.abey@gmail.com>
2025-02-10 15:39:25 +0530

    new book update

--- src/main/java/com/librarymanagementsystem/Book.java
+++ src/main/java/com/librarymanagementsystem/Book.java
@@ -1,14 +1,18 @@
 package com.librarymanagementsystem;
 
 public class Book extends LibraryItem{
+
     public Book(String title, String author, String serialNumber) {
         super(title, author, serialNumber);
     }
 
     @Override
     public void borrowedItem(User user) {
-        isBorrowed = true;
-        System.out.println(user+" is borrowed the book "+getTitle());
-
+        if(!isBorrowed){
+            isBorrowed=true;
+            System.out.println(user+" borrowed the book "+getTitle());
+        }else {
+            System.out.println("The book "+getTitle()+" is already borrowed!");
+        }
     }
 }
